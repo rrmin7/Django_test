cd6bfdfa0be21f448a64edaf88718b79d79dc488
rrmin7 <rlarkdals714@gmail.com>
2024-04-17 23:12:07 +0900

    first commit

--- todo/apps.py
+++ todo/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PollsConfig(AppConfig):
+class TodoListConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'polls'
+    name = 'todo'
